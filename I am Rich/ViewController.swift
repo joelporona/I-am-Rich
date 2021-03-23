b8c1fcc28dfb3fb0fd0e0ffcf1ea701478c897f5
Joel Perez Orona <joelp.orona@gmail.com>
2021-03-23 19:52:29 +0100

    Updated application with icons and now it runs on the iPhone. This version use predefined coordinated, thus, it might not render properly.

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Boruga on 2021-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
